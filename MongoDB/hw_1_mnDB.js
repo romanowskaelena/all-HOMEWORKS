8f675a2f1b29957c5ea5e7913126ce0cc8a1f4cf
Elena Romanowska <romanowska.elena@gmail.com>
2024-04-26 14:18:06 +0200

    mongo+

---
+++ MongoDB/hw_1_mnDB.js
@@ -0,0 +1,77 @@
+// Создать базу данных homeworks.
+
+// В бд homeworks создать коллекцию users и заполнить документами со следующими свойствами: _id, firstname, lastname, age, gender. Используйте следующие данные:
+
+// 1 Анатолий Ушанов 28 m
+// 2 Светлана Демидова 25 f
+// 3 Никита Иванов 33 m
+// 4 Ольга Петрова 22 f
+
+
+// В бд homeworks создать коллекцию vegetables и заполнить документами со следующими свойствами: _id, title, price, count, country. Используйте следующие данные:
+
+// 1 Potato 370 5 Germany
+// 2 Onion 320 3 Poland
+// 3 Tomato 210 9 Spain
+// 4 Carrot 280 4 France
+
+//------hw-------
+// use homeworks
+
+// db.createCollection('users')
+
+// db.users.insertMany([
+// { _id: 1,
+// 	firstname: 'Anatolij',
+// 	lastname: 'Ushanov',
+// 	age: 25,
+// 	gender: 'm'
+// },
+// { _id: 2,
+// 	firstname: 'Svetlana',
+// 	lastname: 'Demidova',
+// 	age: 25,
+// 	gender: 'f'
+// },
+// { _id: 3,
+// 	fistname: 'Nilita',
+// 	lastname: 'Ivanov',
+// 	age: 33,
+// 	gender: 'm'
+// }
+// ]);
+
+// db.createCollection('vegetables')
+
+// db.vegetables.insertMany([
+// { _id: 1,
+// 	title: 'Potato',
+// 	price: '370',
+// 	count: '3',
+// 	country: 'germany'
+// },
+//  { _id: 2,
+//     title: 'Onion',
+//     price: '320',
+//     count: '5',
+//     country: 'poland'
+// },
+// { _id: 3,
+//     title: 'Tomato',
+//     price: '210',
+//     count: '9',
+//     country: 'spain'
+// },
+// { _id: 4,
+//     title: 'carrot',
+//     price: '280',
+//     count: '4',
+//     country: 'france'
+// }
+// ]);
+
+// 
+
+
+
+
